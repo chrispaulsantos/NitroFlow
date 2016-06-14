3c2baf7515c33f84a86a8526c1f8a9f54d51a85a
Chris <chrissantosproduction@gmail.com>
2016-06-14 10:12:01 -0400

    Working on get

--- src/php/get_data.php
+++ src/php/get_data.php
@@ -3,13 +3,13 @@
     require_once "database_connect.php";
     require "location_object.php";
 
-    $stmt = DBConnection::instance()->prepare("SELECT DISTINCT P_Id, current_capacity FROM Location_Data ORDER BY time_stamp DESC");
+    $stmt = DBConnection::instance()->prepare("SELECT DISTINCT P_Id FROM Location ORDER BY time_stamp DESC");
     $stmt->execute();
 
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         $location = new location();
         $location->id = $row["P_Id"];
-        $location->location = $row["current_capacity"];
+        $location->location = $row["location"];
 
         $locs[] = $location;
     }
--- src/php/location_object.php
+++ src/php/location_object.php
@@ -1,8 +1,20 @@
 <?php
+    require_once "database_connect.php";
 
     class location {
 
         public $id;
         public $location;
         public $current_capacity;
+
+        public function insertLocation() {
+            $stmt = DBConnection::instance()->prepare("INSERT INTO Locations('location') VALUES(':loc')");
+            $stmt->bindParam(":loc", $location);
+            $stmt->execute();
+        }
+
+        public function getLocationData() {
+            $stmt = DBConnection::instance()->prepare("SELECT DISTINCT P_Id FROM Location_Data ORDER BY time_stamp DESC LIMIT 1");
+            $stmt->execute();
+        }
     }
