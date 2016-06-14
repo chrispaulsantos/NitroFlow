47e8970f1994b6f5f940b8c9473570eda979279f
Chris <chrissantosproduction@gmail.com>
2016-06-13 22:22:44 -0400

    Added column to query

--- src/php/get_data.php
+++ src/php/get_data.php
@@ -3,7 +3,7 @@
     require_once "database_connect.php";
     require "location_object.php";
 
-    $stmt = DBConnection::instance()->prepare("SELECT DISTINCT P_Id FROM Location_Data ORDER BY time_stamp DESC");
+    $stmt = DBConnection::instance()->prepare("SELECT DISTINCT P_Id, current_capacity FROM Location_Data ORDER BY time_stamp DESC");
     $stmt->execute();
 
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
