1a1bb220e74997390523e6d81e9c675712839eb1
Chris <chrissantosproduction@gmail.com>
2016-06-14 21:11:02 -0400

    Testing get

--- src/php/get_data.php
+++ src/php/get_data.php
@@ -3,21 +3,66 @@
     require_once "database_connect.php";
     require "location_object.php";
 
-    $stmt = DBConnection::instance()->prepare("SELECT DISTINCT `P_Id` FROM `Locations` ORDER BY `time_stamp` DESC");
+
+
+    $args = [];
+    $args[] = {'key':'location','value':'200 Seaport Blvd'};
+    if( $_GET != null ) {
+        $args = $_GET['args'];
+    }
+
+    $stmt = DBConnection::instance()->prepare(constructQuery($args));
     error_log($stmt->execute());
 
-
-
-    $locs = [];
+    $locations = [];
 
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 
-        error_log("In the loop");
-
-        /*$location = new location();
+        $location = new location();
         $location->id = $row["P_Id"];
         $location->location = $row["location"];
-        error_log(json_encode($location));
-        $locs[] = $location;*/
+        $locations[] = $location;
     }
-    //error_log(json_encode($locs));
+
+    $stmt = DBConnection::instance()->prepare("SELECT * FROM `Location_Data` ORDER BY `time_stamp` DESC");
+    error_log($stmt->execute());
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        foreach($locations as $location){
+            if($row["P_Id"] == $location->id){
+                $location->current_capacity = $row["current_capacity"];
+            }
+        }
+    }
+
+    /**
+     * @param $args
+     * @return string
+     * This function constructs a SQL query built off of multiple WHERE parameters. If there is one value in args, it
+     * will return only one WHERE. If there is more than one object in args, it will construct a query containing
+     * multiple WHERE's, and append an AND where it is needed.
+     */
+    function constructQuery( $args ) {
+
+        // Base query (SELECT ALL)
+        $query = "SELECT * FROM Locations ";
+        $ct = 0;
+
+        // If only one value, return query with one value, else return multiple WHERE query
+        if( count( $args) == 1) {
+            $query .= "WHERE " . $args[0]["key"] . " = " . $args[0]["value"];
+        } else if( count( $args) > 1) {
+            $query .= "WHERE ";
+            for($ct = 0; $ct < count($args); $ct++){
+                $query .= $args[$ct]["key"] . " = " . $args[$ct]["value"];
+
+                // Appends an AND up until ct - 1 (Since no AND is needed after the last WHERE
+                if($ct < count( $args) - 1){
+                    $query .= " AND ";
+                }
+            }
+        }
+
+        // Return the constructed query
+        return $query;
+    }
