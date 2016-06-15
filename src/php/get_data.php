231074c56451d9bf5c6021b1c1494b743282d1d3
Chris <chrissantosproduction@gmail.com>
2016-06-15 16:17:38 -0400

    Spaces were throwing the query off

--- src/php/get_data.php
+++ src/php/get_data.php
@@ -13,9 +13,9 @@
     }
     $id = 1;
 
-    $query = "SELECT Locations.P_Id, Locations.location, Location_Data.current_capacity, Location_Data.time_stamp".
-             "FROM Locations JOIN Location_Data ON Locations.P_Id = Location_Data.P_Id".
-             "WHERE Location_Data.time_stamp = (SELECT MAX(Location_Data.time_stamp)".
+    $query = "SELECT Locations.P_Id, Locations.location, Location_Data.current_capacity, Location_Data.time_stamp ".
+             "FROM Locations JOIN Location_Data ON Locations.P_Id = Location_Data.P_Id ".
+             "WHERE Location_Data.time_stamp = (SELECT MAX(Location_Data.time_stamp) ".
              "FROM Location_Data WHERE Location_Data.P_Id = :id) AND Locations.P_Id = :id";
 
     try {
