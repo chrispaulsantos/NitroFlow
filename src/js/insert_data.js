512a323df3b2bbd156846400e2242b5670cc4988
Chris <chrissantosproduction@gmail.com>
2016-06-16 09:29:11 -0400

    Forgot math in front of abs

--- src/js/insert_data.js
+++ src/js/insert_data.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
                 datatype: "text"
             }).done(function(response) {
                 console.log(response);
-                data.datasets[0].data[0] = capacity - 5*abs(Math.sin(rand(1,100)));
+                data.datasets[0].data[0] = capacity - 5*Math.abs(Math.sin(rand(1,100)));
                 data.datasets[0].data[5] = capacity-Math.abs((Math.floor((Math.random() * 15) + 1)));
                 data.datasets[0].data[10] = capacity-Math.abs((Math.floor((Math.random() * 17) + 1)));
                 myBarChart.update();
