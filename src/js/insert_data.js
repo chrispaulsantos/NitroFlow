906c25a0ca7565f40071ef638420a5df5c0d9f74
Chris <chrissantosproduction@gmail.com>
2016-06-16 09:33:45 -0400

    Trying sin

--- src/js/insert_data.js
+++ src/js/insert_data.js
@@ -12,15 +12,15 @@ $(document).ready(function(){
                 datatype: "text"
             }).done(function(response) {
                 console.log(response);
-                data.datasets[0].data[0] = capacity - 5*Math.abs(Math.sin(Math.random(1,100)));
+                capacity = capacity - 5*Math.abs(Math.sin(Math.random(1,100)));
+                data.datasets[0].data[0] = capacity;
                 data.datasets[0].data[5] = capacity-Math.abs((Math.floor((Math.random() * 15) + 1)));
                 data.datasets[0].data[10] = capacity-Math.abs((Math.floor((Math.random() * 17) + 1)));
                 myBarChart.update();
                 //drawGraph(capacity);
             });
-            capacity -= 1;
             console.log(capacity);
-            if(capacity == 0){capacity = 100;}
+            if(capacity < 0){capacity = 100;}
         } else {
             clearInterval(inter);
         }
