d46ba083ed39f65030e607156814ad2e6c26dc7a
Chris <chrissantosproduction@gmail.com>
2016-06-20 22:00:27 -0400

    edited function slightly

--- src/sql/storedProcedures.sql
+++ src/sql/storedProcedures.sql
@@ -1,8 +1,8 @@
 DELIMITER $$
-CREATE FUNCTION `flow_data`.`insertLocation`(loc varchar(255))
+CREATE FUNCTION `flow_data`.`insertLocation`(loc varchar(255), reg INT)
 RETURNS INT
 BEGIN
-  INSERT INTO Locations(location) VALUES (loc);
+  INSERT INTO Locations(location, region) VALUES (loc, reg);
   RETURN LAST_INSERT_ID();
 END $$
 DELIMITER ;
