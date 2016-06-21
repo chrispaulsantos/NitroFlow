3394add80fae17ec5370c869e8dca347a5f82ab5
Chris <chrissantosproduction@gmail.com>
2016-06-21 12:00:57 -0400

    Added procdure to list of procedures

--- src/sql/storedProcedures.sql
+++ src/sql/storedProcedures.sql
@@ -7,3 +7,19 @@ BEGIN
 END $$
 DELIMITER ;
 
+DELIMITER $$
+CREATE PROCEDURE `flow_data`.`getByLocation`(IN id INT)
+LANGUAGE SQL
+DETERMINISTIC
+SQL SECURITY DEFINER
+COMMENT 'Get by location'
+BEGIN
+    SELECT Locations.P_Id, Locations.location, Location_Data.current_capacity, Location_Data.time_stamp
+    FROM Locations
+    JOIN Location_Data
+    ON Locations.P_Id = Location_Data.P_Id
+    WHERE Location_Data.time_stamp = (SELECT MAX(Location-Data.time_stamp)
+                                      FROM Location_Data
+                                      WHERE Location_Data.P_Id = id)
+    AND Locations.P_Id = id;
+END $$
