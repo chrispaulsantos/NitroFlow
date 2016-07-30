da880869b56a417054b9206ebaeae71f17825e8c
Chris <chrissantosproduction@gmail.com>
2016-07-30 14:56:58 -0400

    Switching to unix time stamp

--- src/php/getByRegion.php
+++ src/php/getByRegion.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chris
- * Date: 6/21/16
- * Time: 5:26 PM
- */
 
     require_once "database_connect.php";
     require "location_object.php";
 
     $region = $_GET["region"];
+
 
     try {
         $stmt = DBConnection::instance()->prepare("CALL `getByRegion`(:region)");
