99ae4b42cc5d2880c2fd9cb01e43be5a6a90eb80
Chris <chrissantosproduction@gmail.com>
2016-08-02 13:38:09 -0400

    Defining rows

--- src/php/getByRegion.php
+++ src/php/getByRegion.php
@@ -5,6 +5,7 @@
 
     $stmt = null;
     $locations = [];
+    $rows = [];
 
     // Region passed from js
     if( $_GET != null ) {
@@ -14,8 +15,8 @@
     // Prepare the query for execution
     try {
         $query = "SELECT Current_Data.P_Id, Current_Data.capacity, Locations.region, Locations.location FROM Current_Data INNER JOIN Locations ON Current_Data.P_Id = Locations.P_Id WHERE Locations.region = :reg";
-        //$stmt = DBConnection::instance()->prepare($query);
-        $stmt = DBConnection::instance()->prepare("CALL `getByRegion`(:reg)");
+        $stmt = DBConnection::instance()->prepare($query);
+        //$stmt = DBConnection::instance()->prepare("CALL `getByRegion`(:reg)");
         $stmt->bindParam(":reg", $region);
         $stmt->execute();
     } catch(Exception $e){
