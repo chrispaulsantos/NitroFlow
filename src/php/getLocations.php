e2e6294397a6b6f86f2eeb01616eb30b94b61bed
Chris <chrissantosproduction@gmail.com>
2016-08-02 09:09:19 -0400

    Forgot try catch

--- src/php/getLocations.php
+++ src/php/getLocations.php
@@ -8,8 +8,13 @@
 
     require_once "database_connect.php";
 
+try {
     $stmt = DBConnection::instance()->prepare("SELECT DISTINCT `location` FROM `Locations`");
     $stmt->execute();
+} catch (Exception $e){
+    error_log("Error: ") . $e->getMessage();
+}
+
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         $rows[] = $row;
