4f30c7316a45f1161c11d2faccea2c23ba493029
Chris <chrissantosproduction@gmail.com>
2016-08-02 14:00:31 -0400

    Manually setting region

--- src/php/getByRegion.php
+++ src/php/getByRegion.php
@@ -10,6 +10,7 @@
     // Region passed from js
     if( $_GET != null ) {
         $region = $_GET['region'];
+        $region = 'BOSZ2L';
     }
 
     // Prepare the query for execution
@@ -17,7 +18,7 @@
         $query = "SELECT `Current_Data`.`P_Id`, `Current_Data`.`capacity`, `Locations`.`region`, `Locations`.`location` FROM `flow_data`.`Current_Data` INNER JOIN `flow_data`.`Locations` ON `Current_Data`.`P_Id` = `Locations`.`P_Id` WHERE `Locations`.`region` = :reg";
         //$stmt = DBConnection::instance()->prepare($query);
         $stmt = DBConnection::instance()->prepare("CALL `flow_data`.`getByRegion`(:reg)");
-        $stmt->bindParam(":reg", 'BOSZ2L');
+        $stmt->bindParam(":reg", $region);
         $stmt->execute();
     } catch(Exception $e){
         error_log("Error: " .$e->getMessage());
