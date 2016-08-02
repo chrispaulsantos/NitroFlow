c09a1ae0cf16180b994e862a80dc7b3ce416a93b
Chris <chrissantosproduction@gmail.com>
2016-08-02 12:16:13 -0400

    Putting button on search bar

--- index.php
+++ index.php
@@ -67,10 +67,10 @@
                     <i class="search link icon"></i>
                 </div>
                 <div class="results"></div>
+                <button class="ui button">
+                    <i class="right arrow icon"></i>
+                </button>
             </div>
-            <button class="ui button">
-                Select
-            </button>
             <div class="ui right secondary menu">
                 <div class="ui dropdown pointing item">
                     <i class="options icon"></i>
