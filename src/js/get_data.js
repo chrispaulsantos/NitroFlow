ceef034c7c72f781638ad1b384983277ba1dad79
Chris <chrissantosproduction@gmail.com>
2016-08-02 14:20:56 -0400

    Damn quotes

--- src/js/get_data.js
+++ src/js/get_data.js
@@ -110,7 +110,7 @@ function timeStamp() {
 
 function getByRegion(data, options, myBarChart){
     var region = $("#region").val();
-    $("#chartHolder").empty().append("<canvas id="chart" width="400" height="250"></canvas>");
+    $("#chartHolder").empty().append("<canvas id='chart' width='400' height='250'></canvas>");
     console.log(region);
     // Draw graph initially on pageload
     var ctx = document.getElementById("chart");
