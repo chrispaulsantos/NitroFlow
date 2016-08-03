008b8d0d95efc4e64777c92898cd789d26d70ae6
Chris <chrissantosproduction@gmail.com>
2016-08-03 18:16:25 -0400

    Wrong I think

--- src/js/get_data.js
+++ src/js/get_data.js
@@ -38,10 +38,32 @@ $(document).ready(function() {
     $(document).on("change","#location",function(){
         // variables for drawing the chart; datasets.data and labels initially empty
         var lineData = {
-            labels: [],
+            labels: [],[],
             datasets: [
                 {
                     label: "",
+                    fill: false,
+                    lineTension: 0.7,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [],
+                    spanGaps: false,
+                },
+                {
+                    label: "Second",
                     fill: false,
                     lineTension: 0.7,
                     backgroundColor: "rgba(75,192,192,0.4)",
