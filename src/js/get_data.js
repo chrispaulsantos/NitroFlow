5cdc3b32cd2807a2a3714824c2f20fc56e2b266c
Chris <chrissantosproduction@gmail.com>
2016-08-04 15:27:09 -0400

    Putting dataset struct in function

--- src/js/get_data.js
+++ src/js/get_data.js
@@ -1,27 +1,5 @@
 var int = null;
 var chart = null;
-var datasetStructure = {
-    label: "",
-    fill: false,
-    lineTension: 0.7,
-    backgroundColor: "rgba(75,192,192,0.4)",
-    borderColor: "rgba(75,192,192,1)",
-    borderCapStyle: 'butt',
-    borderDash: [],
-    borderDashOffset: 0.0,
-    borderJoinStyle: 'miter',
-    pointBorderColor: "rgba(75,192,192,1)",
-    pointBackgroundColor: "#fff",
-    pointBorderWidth: 1,
-    pointHoverRadius: 5,
-    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-    pointHoverBorderColor: "rgba(220,220,220,1)",
-    pointHoverBorderWidth: 2,
-    pointRadius: 1,
-    pointHitRadius: 10,
-    data: [],
-    spanGaps: false,
-}
 var lineData = {
     labels: [],
     datasets: []
@@ -243,6 +221,30 @@ function buildBarData(data, obj){
     return data;
 }
 function buildLineData(obj){
+
+    var datasetStructure = {
+        label: "",
+        fill: false,
+        lineTension: 0.7,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [],
+        spanGaps: false,
+    }
+
     //lineData.datasets[0].label = $("#location").val();
     var numDatasets = $("#location").val().length;
     lineData.datasets = [];
