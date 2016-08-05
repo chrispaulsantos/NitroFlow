e21678efd07268fed11d8a4814aaec16696d40e5
Chris <chrissantosproduction@gmail.com>
2016-08-04 20:20:50 -0400

    Testing how much data it can handle

--- src/php/getByLocation.php
+++ src/php/getByLocation.php
@@ -19,7 +19,7 @@
     foreach ($ids as $id){
         array_push($params,$id);
     }
-    $toDate = 1469906320;
+    $toDate = 1469916220;
     $fromDate = 1469906220;
     $questionmarks = str_repeat("?,", count($params)-1) . "?";
     array_push($params, $toDate, $fromDate);
