fed732fdf72dab026e29ccc86ee9b7b4c04a3e90
Chris <chrissantosproduction@gmail.com>
2016-08-05 12:27:59 -0400

    Hmmmmm

--- index.php
+++ index.php
@@ -167,12 +167,12 @@
                 $("#location-alert").hide();
             });
             $( "#slider-range" ).slider({
-                value:100,
+                range: true,
                 min: 0,
                 max: 500,
-                step: 50,
+                values: [ 75, 300 ],
                 slide: function( event, ui ) {
-                    $( "#amount" ).val( "$" + ui.value );
+                    $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
                 }
             });
             $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
