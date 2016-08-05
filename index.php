4f5d6b65d2d9941591177ee390065215cfb2cfd9
Chris <chrissantosproduction@gmail.com>
2016-08-05 13:13:29 -0400

    Will text work?

--- index.php
+++ index.php
@@ -166,15 +166,15 @@
 
             noUiSlider.create(range, {
                 start: [ 20, 80 ], // Handle start position
-                step: 1, // Slider moves in increments of '10'
+                step: 15, // Slider moves in increments of '10'
                 margin: 20, // Handles must be more than '20' apart
                 connect: true, // Display a colored bar between the handles
                 direction: 'ltr', // Put '0' at the bottom of the slider
                 orientation: 'horizontal', // Orient the slider vertically
                 behaviour: 'tap-drag', // Move handle on tap, bar is draggable
                 range: { // Slider can select '0' to '100'
-                    'min': 0,
-                    'max': 100
+                    'min': '1',
+                    'max': '100'
                 },
                 pips: { // Show a scale with the slider
                     mode: 'steps',
