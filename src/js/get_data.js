e856a17158d60c342274c33b07cc205b3f1cc98d
Chris <chrissantosproduction@gmail.com>
2016-08-04 20:40:14 -0400

    Put the if check in the right event handler

--- src/js/get_data.js
+++ src/js/get_data.js
@@ -40,25 +40,25 @@ $(document).ready(function() {
     // Listen on location change
     $(document).on("change","#location",function(){
         // variables for drawing the chart; datasets.data and labels initially empty
-
-        var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        max: 105,
-                        min: 0,
-                        stepSize: 5
-                    }
-                }]
-            }
-        };
-        getByLocation(lineData, options);
+        if($("#fromDate").val() == null || $("#fromDate").val() == "" || $("#toDate").val() == null || $("#toDate").val() == ""){
+            $(".ui.message").removeClass("hidden");
+        } else {
+            var options = {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            max: 105,
+                            min: 0,
+                            stepSize: 5
+                        }
+                    }]
+                }
+            };
+            getByLocation(lineData, options);
+        }
     });
     // Hide/Show inputs based on position of toggle
     $(document).on("change","input[name=graph-type]",function(){
-        if($("#fromDate").val() == null || $("#fromDate").val() == "" || $("#toDate").val() == null || $("#toDate").val() == ""){
-            $(".ui.message").removeClass("hidden");
-        }
         $("#location-holder").toggle();
         $("#region-holder").toggle();
         $("#dates").toggle();
@@ -67,47 +67,51 @@ $(document).ready(function() {
             $("#region-alert").toggle();
         }
     });
-    // On refresh
+    // On refresh click
     $(document).on("click",".icon.refresh",function(){
-        var lineData = {
-            labels: [],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fill: false,
-                    lineTension: 0.1,
-                    backgroundColor: "rgba(75,192,192,0.4)",
-                    borderColor: "rgba(75,192,192,1)",
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: "rgba(75,192,192,1)",
-                    pointBackgroundColor: "#fff",
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    spanGaps: false,
-                }
-            ]
-        };
-        var options = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        max: 100,
-                        min: 0,
-                        stepSize: 10
+        if($("#fromDate").val() == null || $("#fromDate").val() == "" || $("#toDate").val() == null || $("#toDate").val() == ""){
+            $(".ui.message").removeClass("hidden");
+        } else {
+            var lineData = {
+                labels: [],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
                     }
-                }]
-            }
-        };
-        getByLocation(lineData, options);
+                ]
+            };
+            var options = {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            max: 100,
+                            min: 0,
+                            stepSize: 10
+                        }
+                    }]
+                }
+            };
+            getByLocation(lineData, options);
+        }
     });
 });
 
