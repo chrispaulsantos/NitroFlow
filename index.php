3d77840f27d5d9033f95ace70629f969a4ce15cd
Chris <chrissantosproduction@gmail.com>
2016-08-05 12:45:15 -0400

    Using entire jqeury ui

--- index.php
+++ index.php
@@ -65,7 +65,8 @@
         <link rel="stylesheet" href="src/includes/jquery-ui-1.12.0/jquery-ui.min.css">
 
         <script src="src/includes/jquery-1.12.4.min.js"></script>
-        <script src="src/includes/jquery-ui-1.12.0/jquery-ui.js"></script>
+        <!--<script src="src/includes/jquery-ui-1.12.0/jquery-ui.js"></script>-->
+        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
         <script src="src/js/Chart.js" type="text/javascript"></script>
         <script src="src/js/get_data.js" type="text/javascript"></script>
         <script src="src/css/Semantic/semantic.min.js" type="text/javascript"></script>
