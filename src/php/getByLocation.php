806456c88aec3b1bb17e9bddbe50911bcf69afa9
Chris <chrissantosproduction@gmail.com>
2016-08-08 09:28:56 -0400

    Logging i modulo n

--- src/php/getByLocation.php
+++ src/php/getByLocation.php
@@ -48,14 +48,14 @@
 
 
 function getEveryN($objs,$n){
-    $tmp = array();
     $arr = array();
 
     foreach ($objs as $obj){
+        $tmp = array();
         error_log(json_encode($obj->capacity));
         for($i = 0; $i < $n; $i++){
             if($i%$n == 0){
-                error_log("In the if");
+                error_log($i%$n);
                 $tmp[] = $obj->capacity[$i];
                 error_log($obj->capacity[$i]);
             }
