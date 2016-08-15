154a9bc6452ab030be59dd4e8025927b2616171d
Chris <chrissantosproduction@gmail.com>
2016-08-14 21:11:49 -0400

    This should add a white header

--- index.php
+++ index.php
@@ -145,6 +145,7 @@
             <div class="ui container" style="width:500px; height:196px;">
 
                 <form id="addAccForm" class="ui form"> <!-- Add Account Form -->
+                    <h4 class="ui dividing header" style="color: rgb(255,255,255);">Add Account</h4>
                     <div class="field">
                         <div class="two fields">
                             <div class="field">
