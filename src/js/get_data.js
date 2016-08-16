d08f559f51d252a875e3f210735059a3afa2989e
Chris <chrissantosproduction@gmail.com>
2016-08-16 16:21:22 -0400

    Strange

--- src/js/get_data.js
+++ src/js/get_data.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
     // On add account click
     $(document).on("click","#addAccBt",function(){
-        $("#addAccDim").dimmer({closable: false},"show").addClass("active");
+        $("#addAccDim").dimmer("show",{closable: false});
     })
     // On submit order click
     $(document).on("click","#submitOrd",function(){
