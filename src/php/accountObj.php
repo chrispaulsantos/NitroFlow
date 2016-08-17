8a59c5affd1f9bf4c3d38012f550e855a7fee837
Chris <chrissantosproduction@gmail.com>
2016-08-17 17:13:31 -0400

    Fetching assoc

--- src/php/accountObj.php
+++ src/php/accountObj.php
@@ -84,8 +84,7 @@
                 error_log("Error: " . $e->getMessage());
             }
 
-            while($row = $stmt->fetchAll()){
-                error_log("In the while");
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                 error_log(json_encode($row));
             }
         }
