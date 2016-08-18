afd349fff6fb45a691a584c25e68ccfc3ce5b13e
Chris <chrissantosproduction@gmail.com>
2016-08-18 15:00:30 -0400

    Doing html stuff

--- src/php/accountObj.php
+++ src/php/accountObj.php
@@ -44,7 +44,7 @@
                 $l = $this->accUnitCount + $start;
             }
 
-            echo $start . " : " . $l;
+            echo "</br" . $start . " : " . $l;
 
             for($i = $start; $i <= $l; $i++){
 
@@ -64,7 +64,7 @@
                 }
 
                 $this->UIDS[] = "\$UID$" . $zip . $vendorId . $unitNum;
-                echo "\$UID$" . $zip . $vendorId . $unitNum;
+                echo "</br>\$UID$" . $zip . $vendorId . $unitNum;
             }
         }
         public function insertUnregisteredUIDs(){
