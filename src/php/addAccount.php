c47ba25239ae11d6b2e44273813dab96e4eec12d
Chris <chrissantosproduction@gmail.com>
2016-08-18 17:49:17 -0400

    Whoops

--- src/php/addAccount.php
+++ src/php/addAccount.php
@@ -13,12 +13,12 @@
 
         // Create new account object
         $acc = new account();
-        $acc->accName      = $tempAcc["accName"];
-        $acc->accStrAdd    = $tempAcc["accAddress"];
-        $acc->accAptNum    = $tempAcc["accAptNum"];
-        $acc->accState     = $tempAcc["accState"];
-        $acc->accZip       = $tempAcc["accZip"];
-        $acc->currentUnits = $tempAcc["accUnitCount"];
+        $acc->accName        = $tempAcc["accName"];
+        $acc->accStrAdd      = $tempAcc["accAddress"];
+        $acc->accAptNum      = $tempAcc["accAptNum"];
+        $acc->accState       = $tempAcc["accState"];
+        $acc->accZip         = $tempAcc["accZip"];
+        $acc->requestedUnits = $tempAcc["accUnitCount"];
 
         // Check if the account exists, if not, insert
         if(!$acc->checkIfExists()){
