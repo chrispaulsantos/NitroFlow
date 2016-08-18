8ca4a18186a40cd150e4300f55336785bffcc70a
Chris <chrissantosproduction@gmail.com>
2016-08-18 10:29:17 -0400

    Changing defaults

--- src/js/NitroFlow.js
+++ src/js/NitroFlow.js
@@ -10,7 +10,7 @@ var lineData = {
 //Flag used for checking which graph type is being used
 var flag = false;
 
-chart.defaults.global.hover: {mode: 'dataset'};
+chart.defaults.global.hover = {mode: 'dataset'};
 
 $(document).ready(function() {
     //Initialize all css style and jquery handlers theat require the DOM to be built
